0bf2294d5f14976b621b1990c4c7582f8b4b418c
Jks Liu <chinatianma@gmail.com>
2016-11-22 07:41:07 +0800

    Add utils

--- kagurazaka/jks.py
+++ kagurazaka/jks.py
@@ -11,7 +11,22 @@ def final_close(close, *arguments):
     @decorator.decorator
     def final_colse_decorator(f, *args, **kws):
         try:
-            f(*args, **kws)
+            return f(*args, **kws)
         finally:
             close(*arguments)
     return final_colse_decorator
+
+
+def force_arguments_type(*types):
+    """Decorator which try to convert arguments type"""
+    @decorator.decorator
+    def force_arguments_type_decorator(f, *args, **kws):
+        return f(*[typ(arg) for (typ, arg) in zip(types, args)], **kws)
+    return force_arguments_type_decorator
+
+
+def unit(anything):
+    """Do nothing type converter
+
+    Used for force_arguments_type"""
+    return anything
